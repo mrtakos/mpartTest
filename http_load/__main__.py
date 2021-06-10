0dac18553f2685728cd7d9a10f092c88da44adf3
Michael Takos <michael.takos@oracle.com>
2021-06-09 19:21:29 -0700

    scheduler class and function stubs

--- http_load/__main__.py
+++ http_load/__main__.py
@@ -1,8 +1,56 @@
+import asyncio
 import json
 import logging
+import requests
+import sched
+import time
+from threading import Timer
 
 logging.basicConfig()
 logger = logging.getLogger(__name__)
+
+async def fire(url, key):
+    ''' Hits the API
+    '''
+    headers = {
+        "X-Api-Key": key,
+        "Content-Type": "application/json",
+    }
+    requests.get(url=url,headers=headers)
+
+def tick(url, key, rps):
+    ''' runs every second
+    '''
+    for i in range(rps):
+        fire(url,key) 
+
+class RepeatedTimer(object):
+    ''' Scheduler
+    '''
+    def __init__(self, interval, function, *args, **kwargs):
+        self._timer     = None
+        self.interval   = interval
+        self.function   = function
+        self.args       = args
+        self.kwargs     = kwargs
+        self.is_running = False
+        self.start()
+
+    def _run(self):
+        self.is_running = False
+        self.start()
+        self.function(*self.args, **self.kwargs)
+
+    def start(self):
+        if not self.is_running:
+            self._timer = Timer(self.interval, self._run)
+            self._timer.start()
+            self.is_running = True
+
+    def stop(self):
+        self._timer.cancel()
+        self.is_running = False
+
 
 def main():
     ''' Kick off our jobs in input.json
@@ -18,12 +66,14 @@ def main():
         jobs = json.loads(content)
     except Exception as e:
         logger.error(e)
-    
+
     for j in jobs:
         duration = int(j['durationSec'])
-
-        for i in range(duration):
-            print(j['targetRPS'])
+        rt = RepeatedTimer(1, tick, j['serverURL'], j['authKey'], j['targetRPS'])
+        try:
+            time.sleep(duration) # your long-running job goes here...
+        finally:
+            rt.stop()
 
 if __name__ == "__main__":
     main()
